f90c385946d3c3ec7cb854ad3f32bb79130ee430
hanhan <haannn@qq.com>
2023-11-17 01:54:58 +0800

    feat: init

---
+++ interlace.sh
@@ -0,0 +1,15 @@
+title="将 jpg、png 等格式的图片优化为交错后的图片"
+echo "$title start"
+count=0
+for i in $(find $1 -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg') ; do
+interlace=$(identify -quiet -format "%[interlace]" $i)
+if [[ "$interlace" == "PNG" ]] || [[ "$interlace" == "JPEG" ]]; then
+  echo "$i 已交错，continue..."
+    continue
+fi
+convert -interlace plane -quality 80 "$i" "$i";
+count=$((count + 1))
+echo "$i 交错完成✅"
+done
+echo "🌈🌈本次共新增 $count 个图片"
+echo "$title end"
